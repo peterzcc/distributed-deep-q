9dde50c0853f89c5f4396f247326dca7671274a1
ZENG Cancheng <czeng@client100.(none)>
2015-08-25 23:30:07 +0800

    initial

---
+++ dc1.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#$ -S /bin/bash
+
+# specify where the output file should be put
+#$ -o /project/dygroup2/czeng/distributed-deep-q
+
+# specify the working path
+#$ -wd /project/dygroup2/czeng/distributed-deep-q
+
+# email me with this address...
+#$ -M zccust@gmail.com
+# email when the job starts (b) and after the job has been 
+# completed (e)
+#$ -m be
+
+# my real program which should be run
+#/project/dygroup2/czeng/venv/redis-stable/src/redis-server
+cd param-server
+. ~/.local/bin/celery -A tasks worker --loglevel=info
